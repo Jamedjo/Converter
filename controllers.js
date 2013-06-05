50ab1d5fc715aaeff7800d9d3ab7f56237ace5ee
James <git@jamedjo.co.uk>
2013-06-05 08:18:09 +0100

    Salary converter

--- controllers.js
+++ controllers.js
@@ -17,6 +17,7 @@ function updateOnResize($window, $scope) {
   $scope.height = $window.innerHeight;
   $scope.lt550 = ($window.innerWidth<550);
 }
+
 Converter.run(function ($window, $rootScope) {
   updateOnResize($window, $rootScope);
   angular.element($window).bind('resize', function () {
@@ -34,3 +35,25 @@ Converter.filter('ifTrue', function() {
       return "";
     }
   });
+
+function SalaryConverter ($scope) {
+  $scope.salary=10000;
+  $scope.workingHours=35;
+  $scope.daysInWorkingWeek=5;
+  $scope.salaryPeriods = [
+     {name:"Daily", days:function(daysInWorkingWeek){return 1;}}
+    ,{name:"Weekly", days:function(daysInWorkingWeek){return daysInWorkingWeek;}}
+    ,{name:"4-weeks", days:function(daysInWorkingWeek){return daysInWorkingWeek * 4;}}
+    ,{name:"Monthly", days:function(daysInWorkingWeek){return (daysInWorkingWeek * 52)/12; }}
+    ,{name:"Annnual", days:function(daysInWorkingWeek){return daysInWorkingWeek * 52; }}
+  ];
+  $scope.activeSalaryPeriod = "Annnual";
+  $scope.setSalaryPeriod = function(period){$scope.activeSalaryPeriod=period;};
+  $scope.convertedSalary = function() {
+    return $scope.salaryPeriods.filter(function(item){
+      return item.name==$scope.activeSalaryPeriod;
+    })[0].days($scope.daysInWorkingWeek);
+    return $scope.salary;
+    // into per day 
+  };
+}
